d790c1d44ed29d0b6d5f0c00800c08ef8b80d7e1
Eman <emanabukhater2001@gmail.com>
2025-06-03 23:33:44 +0300

    move the dashboard view to components folder

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
-import DashboardView from '@/components/dashboard/DashboardView';
+import DashboardView from '../../components/DashboardView';
 
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
