c020d05c8bff024a860787bcf00c65a64ef1effa
Eman <emanabukhater2001@gmail.com>
2025-06-04 22:57:57 +0300

    Wrap  App with <SessionProvider>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
 "use client";
 
 import { ThemeContextProvider } from '@/context/ThemeContext';
+import { SessionProvider } from "next-auth/react";
+
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
 
+      <SessionProvider>
         <ThemeContextProvider>
           {children}
         </ThemeContextProvider>
+      </SessionProvider>
       </body>
     </html>
   );
