76dd57564bffec004b2592cfe58c951ebed282b1
Eman <emanabukhater2001@gmail.com>
2025-06-10 23:12:05 +0300

    Create PrintButton

---
+++ src/components/invoice/PrintButton.jsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+import PrintIcon from "@mui/icons-material/Print";
+
+export default function PrintButton() {
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      startIcon={<PrintIcon />}
+      onClick={() => window.print()}
+      sx={{ displayPrint: "none" }}
+    >
+      Print
+    </Button>
+  );
+}
